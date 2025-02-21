7e4590f0be2dbef63142942a0a7d6b317dc0d364
Nicolás <nicolascorrod@gmail.com>
2025-02-21 16:24:18 +0100

    Forms and profiles view

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,8 +1,11 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import LoginView from './views/LoginView'
 import RegisterView from './views/RegisterView'
 import AuthLayout from './layouts/AuthLayout'
-
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import AppLayout from './layouts/AppLayout'
+import ProfileView from './views/ProfileView'
+import LinkTreeView from './views/LinkTreeView'
 
 export default function Router() {
     return (
@@ -13,6 +16,10 @@ export default function Router() {
                     {/* Route Sons */}
                     <Route path='auth/login' element={<LoginView />} />
                     <Route path='auth/register' element={<RegisterView />} />
+                </Route>
+                <Route path='/admin' element={<AppLayout/>}>
+                    <Route index={true} element={<LinkTreeView />}/>
+                    <Route path='profile' element={<ProfileView />}/>
                 </Route>
             </Routes>
         </BrowserRouter>
